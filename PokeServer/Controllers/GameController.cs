586f3bcebc755ebfad1cf6ff48d7559f3d6a6ab6
Ryan Luttrull <reluttrull@gmail.com>
2025-10-17 08:44:44 -0500

    for now, store new game data in IMemoryCache

--- PokeServer/Controllers/GameController.cs
+++ PokeServer/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using PokeServer.Model;
 
 namespace PokeServer.Controllers
@@ -8,10 +9,12 @@ namespace PokeServer.Controllers
     public class GameController : ControllerBase
     {
         private readonly ILogger<GameController> _logger;
+        private readonly IMemoryCache _memoryCache;
 
-        public GameController(ILogger<GameController> logger)
+        public GameController(ILogger<GameController> logger, IMemoryCache memoryCache)
         {
             _logger = logger;
+            _memoryCache = memoryCache;
         }
 
         [HttpGet]
@@ -20,15 +23,29 @@ namespace PokeServer.Controllers
         {
             Game game = new Game(DeckId);
             _logger.LogInformation("New game created with DeckId: {DeckId}", DeckId);
+            // TODO: retrieve each card from cache if stored (reduce API calls)
             if (game.Deck.Cards.Count == 0)
             {
                 _logger.LogInformation("Populating card list for deck {DeckId}", DeckId);
                 game.Deck.Cards = await ApiHelper.PopulateCardList(game.Deck.CardIds);
                 _logger.LogInformation("Populated {CardCount} cards for deck {DeckId}", game.Deck.Cards.Count, DeckId);
             }
-            // TODO: retrieve each card from cache or API
-            // TODO: persist game state to data store
-            return game.Deck.Name; // proving deck deserialization was successful
+            // TODO: switch to Redis?
+            if (!_memoryCache.TryGetValue(game.Guid.ToString(), out Game? value))
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromHours(3));
+                _memoryCache.Set<Game>(game.Guid.ToString(), game, cacheEntryOptions);
+            }
+            return game.Guid.ToString(); // TODO: return game start data along with Guid
+        }
+
+        [HttpGet]
+        [Route("checkgameactive/{guid}")]
+        public async Task<bool> CheckGameActive(string guid)
+        {
+            if (_memoryCache.TryGetValue(guid, out Game? value)) return true;
+            return false;
         }
     }
 }
