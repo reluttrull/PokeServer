afae1bfe0cb3df438a2f2b3f44dad3f2ad3097bd
Ryan Luttrull <reluttrull@gmail.com>
2025-10-17 14:07:27 -0500

    cache card data with Redis, and only call API for cards that aren't found in cache.  still need to set rules for how long cache items stick around.

--- PokeServer/ApiHelper.cs
+++ PokeServer/ApiHelper.cs
@@ -1,5 +1,6 @@
 ﻿using PokeServer.Model;
 using System.Text.Json.Nodes;
+using StackExchange.Redis;
 
 namespace PokeServer
 {
@@ -7,55 +8,67 @@ namespace PokeServer
     {
         public static async Task<List<Card>> PopulateCardList(List<string> cardIds)
         {
+            string connectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+            var redis = ConnectionMultiplexer.Connect(connectionString);
+            IDatabase db = redis.GetDatabase();
+
             List<Card> cards = new List<Card>();
             for (int i = 0; i < cardIds.Count; i++)
             {
-                HttpResponseMessage response = await new HttpClient().GetAsync($"https://api.tcgdex.net/v2/en/cards/{cardIds[i]}");
-                if (response.IsSuccessStatusCode)
+                string cardJson = "";
+                if (db.KeyExists(cardIds[i])) // don't need to call api
                 {
-                    string jsonResponse = await response.Content.ReadAsStringAsync();
-                    var options = new System.Text.Json.JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true,
-                    };
-                    var root = JsonNode.Parse(jsonResponse)!;
-                    var jsonCategory = root["category"]?.GetValue<string>();
-                    Enums.CardCategory category = (Enums.CardCategory)Enum.Parse(typeof(Enums.CardCategory), jsonCategory);
-                    switch (category)
-                    {
-                        case Enums.CardCategory.Pokemon:
-                            PokemonCard pCard = System.Text.Json.JsonSerializer.Deserialize<PokemonCard>(jsonResponse, options);
-                            if (pCard != null)
-                            {
-                                cards.Add(pCard);
-                            }
-                            break;
-                        case Enums.CardCategory.Trainer:
-                            TrainerCard tCard = System.Text.Json.JsonSerializer.Deserialize<TrainerCard>(jsonResponse, options);
-                            if (tCard != null)
-                            {
-                                cards.Add(tCard);
-                            }
-                            break;
-                        case Enums.CardCategory.Energy:
-                            EnergyCard eCard = System.Text.Json.JsonSerializer.Deserialize<EnergyCard>(jsonResponse, options);
-                            if (eCard != null)
-                            {
-                                cards.Add(eCard);
-                            }
-                            break;
-                        default:
-                            Card oCard = System.Text.Json.JsonSerializer.Deserialize<Card>(jsonResponse, options);
-                            if (oCard != null)
-                            {
-                                cards.Add(oCard);
-                            }
-                            break;
-                    }
+                    cardJson = db.StringGet(cardIds[i]);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine($"calling API for {cardIds[i]}");
+                    HttpResponseMessage response = await new HttpClient().GetAsync($"https://api.tcgdex.net/v2/en/cards/{cardIds[i]}");
+                    if (!response.IsSuccessStatusCode) throw new Exception("failed to retrieve card data from TCGDex API");
+                    cardJson = await response.Content.ReadAsStringAsync();
+                    db.StringSet(cardIds[i], cardJson);
+                }
+                var options = new System.Text.Json.JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                var root = JsonNode.Parse(cardJson)!;
+                var jsonCategory = root["category"]?.GetValue<string>();
+                Enums.CardCategory category = (Enums.CardCategory)Enum.Parse(typeof(Enums.CardCategory), jsonCategory);
+                switch (category)
+                {
+                    case Enums.CardCategory.Pokemon:
+                        PokemonCard pCard = System.Text.Json.JsonSerializer.Deserialize<PokemonCard>(cardJson, options);
+                        if (pCard != null)
+                        {
+                            cards.Add(pCard);
+                        }
+                        break;
+                    case Enums.CardCategory.Trainer:
+                        TrainerCard tCard = System.Text.Json.JsonSerializer.Deserialize<TrainerCard>(cardJson, options);
+                        if (tCard != null)
+                        {
+                            cards.Add(tCard);
+                        }
+                        break;
+                    case Enums.CardCategory.Energy:
+                        EnergyCard eCard = System.Text.Json.JsonSerializer.Deserialize<EnergyCard>(cardJson, options);
+                        if (eCard != null)
+                        {
+                            cards.Add(eCard);
+                        }
+                        break;
+                    default:
+                        Card oCard = System.Text.Json.JsonSerializer.Deserialize<Card>(cardJson, options);
+                        if (oCard != null)
+                        {
+                            cards.Add(oCard);
+                        }
+                        break;
                 }
             }
             return cards;
-        }   
+        }
         public static async Task<PokemonCard> PopulatePokemonCardInfo(Card card)
         {
             HttpResponseMessage response = await new HttpClient().GetAsync($"https://api.tcgdex.net/v2/en/cards/{card.Id}");
