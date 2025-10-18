e19cd460415815ec2a34a5c4d6ba05bdd80d48b5
Ryan Luttrull <reluttrull@gmail.com>
2025-10-17 19:42:27 -0500

    match image url property name (wasn't getting populated)

--- PokeServer/Model/Card.cs
+++ PokeServer/Model/Card.cs
@@ -8,7 +8,7 @@ namespace PokeServer.Model
         public Guid DeckNumber { get; set; } = Guid.NewGuid();
         public string Category { get; set; } = string.Empty;
         public string Id { get; set; } = string.Empty;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
     }
