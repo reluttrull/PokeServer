dce8815914f4430a5312cf8056d3444beb36f0b6
Ryan Luttrull <reluttrull@gmail.com>
2025-10-17 19:24:36 -0500

    remove container, update debug settings, use openapi in all environments

--- PokeServer/Program.cs
+++ PokeServer/Program.cs
@@ -14,10 +14,7 @@ builder.Services.AddMemoryCache();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
