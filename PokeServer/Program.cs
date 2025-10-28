fece685ac0326181669aa5a6a904f2c3e60860aa
Ryan Luttrull <reluttrull@gmail.com>
2025-10-28 13:51:01 -0500

    Add basic Azure health check endpoint

--- PokeServer/Program.cs
+++ PokeServer/Program.cs
@@ -7,10 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddMemoryCache(); // inject in-memory caching service
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 app.MapOpenApi();
+app.MapHealthChecks("/health");
 
 app.UseHttpsRedirection();
 
