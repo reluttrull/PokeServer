5b6d6fcd70385e3588c4479f9b0782e8a14b004b
Ryan Luttrull <reluttrull@gmail.com>
2025-11-01 09:40:27 -0500

    created deck controller, and one endpoint so far, to return all deck summaries (leaving out specific card info)

---
+++ PokeServer/Controllers/DeckController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using PokeServer.Model;
+using System.Text.Json;
+
+namespace PokeServer.Controllers
+{
+    [ApiController]
+    [Route("deck")]
+    public class DeckController : ControllerBase
+    {
+        private readonly ILogger<DeckController> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        public DeckController(ILogger<DeckController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        [Route("getalldeckbriefs")]
+        public async Task<List<DeckBrief>> GetAllDeckBriefs()
+        {
+            List<DeckBrief> deckBriefs = new();
+
+
+            using (StreamReader r = new StreamReader("TestData/TestDecks.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Deck> decks = JsonSerializer.Deserialize<List<Deck>>(json);
+                foreach (Deck deck in decks)
+                {
+                    DeckBrief deckBrief = new DeckBrief
+                    {
+                        DeckId = deck.DeckId,
+                        Name = deck.Name,
+                        Description = deck.Description
+                    };
+                    deckBriefs.Add(deckBrief);
+                }
+            }
+            return deckBriefs;
+        }
+    }
+}
