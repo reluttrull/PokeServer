bd19b64ebfeebeaad832c58d71e6de09c82215fc
Ryan Luttrull <reluttrull@gmail.com>
2025-11-05 09:16:24 -0600

    Stubbed out basic game history that client will be able to show for user reference

---
+++ PokeServer/Model/GameLog.cs
@@ -0,0 +1,26 @@
+﻿namespace PokeServer.Model
+{
+    public class GameLog
+    {
+        public Enums.GameEvent EventType { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<string>? InvolvedCardIds { get; set; }
+        public GameLog(Enums.GameEvent eventType)
+        {
+            EventType = eventType;
+            Timestamp = DateTime.UtcNow;
+        }
+        public GameLog(Enums.GameEvent eventType, List<Card> involvedCards)
+        {
+            EventType = eventType;
+            InvolvedCardIds = involvedCards.Select(c => c.NumberInDeck.ToString()).ToList();
+            Timestamp = DateTime.UtcNow;
+        }
+        public GameLog(Enums.GameEvent eventType, Card involvedCard)
+        {
+            EventType = eventType;
+            InvolvedCardIds = new List<string> { involvedCard.NumberInDeck.ToString() };
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
