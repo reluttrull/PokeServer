c2cbe939607301e690988c11e519a015dbe37f29
Ryan Luttrull <reluttrull@gmail.com>
2025-11-13 12:52:27 -0600

    changed to handle when Limitless decklist only shows generic energy description, e.g. 8 Water Energy Energy 20.  works for base set energy types

---
+++ PokeServer/DeckData/EnergyTranslation.cs
@@ -0,0 +1,17 @@
+﻿namespace PokeServer.DeckData
+{
+    public static class EnergyTranslation
+    {
+        public static readonly Dictionary<string, string> EnergyCodes = new Dictionary<string, string>
+        {
+            {"Double", "base1-96"},
+            {"Fighting", "base1-97"},
+            {"Fire", "base1-98"},
+            {"Grass", "base1-99"},
+            {"Lightning", "base1-100"},
+            {"Psychic", "base1-101"},
+            {"Water", "base1-102"}
+            // Add more energy types as needed
+        };
+    }
+}
