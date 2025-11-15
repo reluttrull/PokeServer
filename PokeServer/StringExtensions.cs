cc023a8a583ee19b95b2d6fd1366c9347fcbe040
Ryan Luttrull <reluttrull@gmail.com>
2025-11-15 09:33:34 -0600

    TCGdex API has many evolution Pokemon missing an evolveFrom field.  In this case, we will try to find another card with the same name that DOES have the field populated, add that property, and save it to the cache so we have the value going forward.

---
+++ PokeServer/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace PokeServer.Extensions
+{
+    static class StringExtensions
+    {
+        public static string TrimEnding(this string source, string value)
+        {
+            if (!source.EndsWith(value, StringComparison.OrdinalIgnoreCase))
+                return source;
+
+            return source.Remove(source.LastIndexOf(value));
+        }
+    }
+}
